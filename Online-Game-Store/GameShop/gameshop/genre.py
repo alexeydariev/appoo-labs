9d61749cb53b4cacaf181dc0ca184616cf4495a8
Alexei Dariev <alexeydariev@gmail.com>
2017-06-21 15:58:57 +0300

    final commit

---
+++ Online-Game-Store/GameShop/gameshop/genre.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+"""Game genres"""
+
+GENRE_CHOICES = (
+	('SH', 'Shooter'),
+	('AA', 'Action-adventure'),
+	('RPG', 'Role-playing game'),
+	('SP', 'Sports'),
+	('RTS', 'Real-time strategy'),
+	('HR', 'Horror'),
+	('MMO', 'Massive multiplayer online'),
+	)
